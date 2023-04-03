668abe1722e9e0b61330012993c9b29c6305207b
Dipankar <dipankar15-11137@diu.edu.bd>
2023-04-03 12:59:52 +0600

    design noble

--- src/Components/Pages/Home/HomeSection/Novel/Novel.js
+++ src/Components/Pages/Home/HomeSection/Novel/Novel.js
@@ -3,18 +3,25 @@ import "../../../../CSS/NovelStyle.css";
 
 const Novel = () => {
   return (
-    <div style={{ height: "450px", width: "296px" }} class="container bg-white">
+    <div
+      style={{ height: "500px", width: "296px" }}
+      class="container bg-white shadow-inner"
+    >
       {/* <img src="img_avatar.png" alt="Avatar" class="image" style="width:100%"> */}
       <img
         className="image "
         src="https://benthambooks.com/ebook-images/sample-files-images/9789377871499/9789377871499-1.jpg"
         alt=""
       />
-      <div>
-        <h1>hello ki</h1>
+      <div className="text-center text-xl mt-5">
+        <h1 className="font-bold text-2xl">Noble</h1>
+        <h2 className="font-semibold ">Writer Name</h2>
+        <p>Price</p>
+        <p>Condition : New</p>
+        <p>Publication Date</p>
       </div>
-      <div class="middle">
-        {/* <div class="text">John Doe</div> */}
+      <div class="middle flex justify-between">
+        <button className="btn mr-20"> Book</button>
         <button className="btn">Buy</button>
       </div>
     </div>
--- src/Components/Pages/Home/HomeSection/Novel/Novels.js
+++ src/Components/Pages/Home/HomeSection/Novel/Novels.js
@@ -4,7 +4,7 @@ import Novel from "./Novel";
 const Novels = () => {
   return (
     <div className="mt-20 mx-20">
-      <div className="bg-slate-200 rounded-xl">
+      <div className=" rounded-xl">
         <h1>Novel Books</h1>
         <div className="p-4">
           <Novel />
