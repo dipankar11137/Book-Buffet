e2e2c43da87dab7bf1d4fa63332aa021f7e3a7e3
Dipankar <dipankar15-11137@diu.edu.bd>
2023-04-03 16:14:07 +0600

    add products

--- src/Components/Pages/Home/HomeSection/Novel/Novel.js
+++ src/Components/Pages/Home/HomeSection/Novel/Novel.js
@@ -13,14 +13,14 @@ const Novel = () => {
         src="https://benthambooks.com/ebook-images/sample-files-images/9789377871499/9789377871499-1.jpg"
         alt=""
       />
-      <div className="text-center text-xl mt-5">
+      <div className="text-center text-xl mt-5 testStyle">
         <h1 className="font-bold text-2xl">Noble</h1>
         <h2 className="font-semibold ">Writer Name</h2>
         <p>Price</p>
         <p>Condition : New</p>
         <p>Publication Date</p>
       </div>
-      <div class="middle flex justify-between">
+      <div class="middle flex justify-between ">
         <button className="btn mr-20"> Book</button>
         <button className="btn">Buy</button>
       </div>
--- src/Components/Pages/Home/HomeSection/Novel/Novels.js
+++ src/Components/Pages/Home/HomeSection/Novel/Novels.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Novel from "./Novel";
 
 const Novels = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
   return (
     <div className="mt-20 mx-20 shadow-xl p-3">
       <div className=" rounded-xl">
-        <h1 className="text-4xl font-bold ">Novel Books</h1>
-        <div className="py-4">
-          <Novel />
+        <h1 className="text-4xl font-bold ">Novel Books {products.length}</h1>
+        <div className="py-4 grid grid-cols-5 gap-y-8">
+          {products.map((product) => (
+            <Novel key={product._id} product={product}></Novel>
+          ))}
         </div>
       </div>
     </div>
