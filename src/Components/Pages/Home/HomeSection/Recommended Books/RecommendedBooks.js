2ee545c8c38a473982f0b51f252f7d5b5f3ad2b7
Dipankar <dipankar15-11137@diu.edu.bd>
2023-04-12 21:10:52 +0600

    some design change

--- src/Components/Pages/Home/HomeSection/Recommended Books/RecommendedBooks.js
+++ src/Components/Pages/Home/HomeSection/Recommended Books/RecommendedBooks.js
@@ -4,10 +4,10 @@ import RecommendedBook from "./RecommendedBook";
 const RecommendedBooks = () => {
   return (
     <div className="mt-20 mx-20">
-      <h1 className="text-6xl py-6 bg-gradient-to-r from-purple-100 to-pink-100 text-red-600">
+      <h1 className="pl-5 rounded-xl text-6xl py-6 bg-gradient-to-r from-purple-100 to-pink-100 text-red-600">
         Recommended Books
       </h1>
-      <div className="">
+      <div className="mt-10">
         <RecommendedBook />
       </div>
     </div>
