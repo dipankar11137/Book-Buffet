6afd423cca33b7aa55a8c06bf3588b2f3aef3d7c
Dipankar <dipankar15-11137@diu.edu.bd>
2023-05-02 17:35:44 +0600

    manage books 90%

--- src/Components/Pages/Dashboard/ManageItem/ManageItem.js
+++ src/Components/Pages/Dashboard/ManageItem/ManageItem.js
@@ -1,10 +1,28 @@
 import React from "react";
 
-const ManageItem = () => {
+const ManageItem = ({ product, index, handleDelete }) => {
+  const { _id, name, img, booksCategory, price, status, date } = product;
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <tr>
+      <th>{index}</th>
+      <th>
+        <img className="h-10 w-10 rounded-full" src={img} alt="" />
+      </th>
+      <td>{name}</td>
+      <td>{booksCategory}</td>
+      <td>{price}</td>
+      <td>{status}</td>
+      <td>{date}</td>
+      <td>
+        {" "}
+        <button
+          onClick={() => handleDelete(_id)}
+          className="btn-sm rounded-lg bg-red-800 text-white uppercase"
+        >
+          Delete
+        </button>
+      </td>
+    </tr>
   );
 };
 
--- src/Components/Pages/Dashboard/ManageItem/ManageItems.js
+++ src/Components/Pages/Dashboard/ManageItem/ManageItems.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ManageItem from "./ManageItem";
 
 const ManageItems = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/books")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
   return (
     <div>
-      <h1>Hello</h1>
+      <div className="overflow-x-auto">
+        <table className="table table-compact w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th></th>
+              <th>Name</th>
+              <th>Books Category</th>
+              <th>Price</th>
+              <th>Status</th>
+              <th>Date</th>
+              <th>Remove</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product, index) => (
+              <ManageItem
+                key={product._id}
+                product={product}
+                index={index + 1}
+              ></ManageItem>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
