542ac738a6e091396f87283113294cdd3410f754
Dipankar <dipankar15-11137@diu.edu.bd>
2023-05-16 00:35:40 +0600

    edit text

--- src/Components/Pages/Test/Test.js
+++ src/Components/Pages/Test/Test.js
@@ -3,11 +3,11 @@ import "../../CSS/Test.css";
 
 const Test = () => {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center shadow-2xl">
       <div class="container">
         <div class="row">
           <div class="col-md-12 text-center">
-            <h3 class="animate-charcter font-extrabold"> BookBuffet</h3>
+            <h3 class="animate-charcter font-extrabold "> BookBuffet</h3>
           </div>
         </div>
       </div>
